100a7fad08a05b13101d40d38664a8d9c7ea78bc
RevelationOrange <RevelationOrange@gmail.com>
2018-06-11 11:32:51 -0400

    random stats generation finished stat mods added all updated in the respective stat boxes

--- app/src/main/java/com/example/revelationorange/dndcharactergen/DndChar.java
+++ app/src/main/java/com/example/revelationorange/dndcharactergen/DndChar.java
@@ -1,13 +1,17 @@
 package com.example.revelationorange.dndcharactergen;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 import java.util.Random;
 
 public class DndChar {
     Random rng = new Random();
+    private static final int nBaseStats = 6;
+    private static final int nD6Rolls = 4;
     String chName, plName, chClass, race;
-    List<Integer> baseStats = new ArrayList<>(6);
+    List<Integer> baseStats = new ArrayList<>(nBaseStats);
+    List<Integer> baseStatMods = new ArrayList<>(nBaseStats);
     Integer Str, Dex, Con, Int, Wis, Cha, level, xp, speed, initiative, ac, hitDice;
 
     DndChar() {
@@ -16,10 +20,22 @@ public class DndChar {
     }
 
     void rollStats() {
-        for (int i = 0; i < baseStats.size(); i++) {
-            baseStats.set(i, 7);
+        this.baseStats.clear();
+        this.baseStatMods.clear();
+        List<Integer> statRolls = new ArrayList<>();
+        int statSum = 0;
+        for (int i = 0; i < nBaseStats; i++) {
+            for (int j = 0; j < nD6Rolls; j++) { statRolls.add(rng.nextInt(6)+1); }
+            Collections.sort(statRolls, Collections.<Integer>reverseOrder());
+            for (int j = 0; j < nD6Rolls-1; j++) { statSum += statRolls.get(j); }
+            this.baseStats.add(statSum);
+            this.baseStatMods.add((int) Math.floor((statSum-10)/2));
+            statRolls.clear();
+            statSum = 0;
         }
     }
 
     public List<Integer> getBaseStats() { return this.baseStats; }
+
+    public List<Integer> getBaseStatMods() { return baseStatMods; }
 }
--- app/src/main/java/com/example/revelationorange/dndcharactergen/MainActivity.java
+++ app/src/main/java/com/example/revelationorange/dndcharactergen/MainActivity.java
@@ -17,9 +17,48 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void rollStats(View v) {
+        globalChar.rollStats();
         List<Integer> bStats = globalChar.getBaseStats();
+        List<Integer> bStatMods = globalChar.getBaseStatMods();
 
         TextView statBox = findViewById(R.id.strVal);
-        statBox.setText(bStats.get(0));
+        statBox.setText(String.format(bStats.get(0).toString()));
+        statBox = findViewById(R.id.dexVal);
+        statBox.setText(String.format(bStats.get(1).toString()));
+        statBox = findViewById(R.id.conVal);
+        statBox.setText(String.format(bStats.get(2).toString()));
+        statBox = findViewById(R.id.intVal);
+        statBox.setText(String.format(bStats.get(3).toString()));
+        statBox = findViewById(R.id.wisVal);
+        statBox.setText(String.format(bStats.get(4).toString()));
+        statBox = findViewById(R.id.chaVal);
+        statBox.setText(String.format(bStats.get(5).toString()));
+
+        int mod;
+        String pm;
+        statBox = findViewById(R.id.strMod);
+        mod = bStatMods.get(0);
+        pm = mod<0 ? "" : "+";
+        statBox.setText(String.format("%s%s", pm, mod));
+        statBox = findViewById(R.id.dexMod);
+        mod = bStatMods.get(1);
+        pm = mod<0 ? "" : "+";
+        statBox.setText(String.format("%s%s", pm, mod));
+        statBox = findViewById(R.id.conMod);
+        mod = bStatMods.get(2);
+        pm = mod<0 ? "" : "+";
+        statBox.setText(String.format("%s%s", pm, mod));
+        statBox = findViewById(R.id.intMod);
+        mod = bStatMods.get(3);
+        pm = mod<0 ? "" : "+";
+        statBox.setText(String.format("%s%s", pm, mod));
+        statBox = findViewById(R.id.wisMod);
+        mod = bStatMods.get(4);
+        pm = mod<0 ? "" : "+";
+        statBox.setText(String.format("%s%s", pm, mod));
+        statBox = findViewById(R.id.chaMod);
+        mod = bStatMods.get(5);
+        pm = mod<0 ? "" : "+";
+        statBox.setText(String.format("%s%s", pm, mod));
     }
 }
